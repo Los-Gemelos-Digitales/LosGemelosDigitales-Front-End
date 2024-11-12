b1ec3c0dbe3eaf4e9403d5e4c70c93d61cbc9b96
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2024-11-12 14:05:43 -0500

    update reports and add service and models of: reports and evacuation plan

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
+import {Reports} from '../../data/models/reports/reports';
+import {ReportsService} from '../../data/services/reports/reports.service';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [CommonModule, ToolbarComponent], 
+  imports: [CommonModule, ToolbarComponent, MatTableModule, MatButtonModule],
   templateUrl: './reports.component.html',
   styleUrls: ['./reports.component.css']
 })
-export class ReportsComponent {
-  reports = [
-    { title: 'Report 1', description: 'Description 1', time: '08:00', day: 'Monday', place: 'Classroom 1' },
-    { title: 'Report 2', description: 'Description 2', time: '09:00', day: 'Tuesday', place: 'Classroom 2' },
-    { title: 'Report 3', description: 'Description 3', time: '10:00', day: 'Wednesday', place: 'Classroom 3' },
-    { title: 'Report 4', description: 'Description 4', time: '11:00', day: 'Thursday', place: 'Classroom 4' },
-    { title: 'Report 5', description: 'Description 5', time: '12:00', day: 'Friday', place: 'Classroom 5' },
-    { title: 'Report 6', description: 'Description 6', time: '13:00', day: 'Monday', place: 'Classroom 6' },
-    { title: 'Report 7', description: 'Description 7', time: '14:00', day: 'Tuesday', place: 'Classroom 7' },
-    { title: 'Report 8', description: 'Description 8', time: '15:00', day: 'Wednesday', place: 'Classroom 8' },
-    { title: 'Report 9', description: 'Description 9', time: '16:00', day: 'Thursday', place: 'Classroom 9' },
-  ];
+export class ReportsComponent implements OnInit{
+  reports: Reports[] = [];
+  displayedColumns: string[] = ['id', 'type', 'description', 'date', 'place', 'details'];
+
+  constructor(private reportService: ReportsService) {}
+
+  ngOnInit(): void {
+    this.reportService.getList().subscribe(data => {
+      this.reports = data;
+    });
+  }
 }
---
+++ src/app/data/models/reports/reports.ts
@@ -0,0 +1,8 @@
+export interface Reports {
+  id: number;
+  admin_id: number;
+  type: string;
+  description: string;
+  date: string;
+  place: string;
+}
