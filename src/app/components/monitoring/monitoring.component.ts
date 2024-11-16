f17b4b97502e39afd9298201303b66fb1ca68a57
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2024-11-16 00:46:34 -0500

    update monitoring component

--- src/app/components/monitoring/monitoring.component.ts
+++ src/app/components/monitoring/monitoring.component.ts
@@ -1,23 +1,18 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import {AuthService} from '../../shared/auth-service/auth.service';
+import {Router} from '@angular/router';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-monitoring',
   standalone: true,
-  imports: [ToolbarComponent],
+  imports: [ToolbarComponent, MatButton],
   templateUrl: './monitoring.component.html',
   styleUrl: './monitoring.component.css'
 })
 export class MonitoringComponent {
-  username: string | null = null;
-
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    const user = this.authService.getUser(); // Obtener el usuario desde el AuthService
-    if (user) {
-      this.username = user.username; // Asignar el username
-    }
+  openAzurePortal(): void {
+    window.open('https://portal.azure.com', '_blank'); // Abre en una nueva pestaña
   }
 }
