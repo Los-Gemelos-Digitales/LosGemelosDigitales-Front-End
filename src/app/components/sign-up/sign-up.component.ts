60fc4812203dfbdf7d5678ef5588ac5f39d6b67a
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2024-11-17 15:52:07 -0500

    update sign up

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -2,32 +2,36 @@ import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Users} from '../../data/models/users/user';
 import {UserService} from '../../data/services/users/user.service';
+import {Router} from '@angular/router';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
   user: Users = {
-    id: 0, // Deberías asignar el id automáticamente desde el backend si es necesario.
+    id: 0,
     email: '',
     username: '',
     password: '',
     role: 'teacher', // 'teacher' es el valor por defecto
   };
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   onSubmit(): void {
     this.userService.createItem(this.user).subscribe({
       next: (data) => {
         console.log('User created:', data);
-        // Redirigir a otra página o mostrar un mensaje de éxito.
+        // Redirigir al login después de la creación
+        this.router.navigate(['/login']);
       },
       error: (err) => {
         console.error('Error creating user:', err);
